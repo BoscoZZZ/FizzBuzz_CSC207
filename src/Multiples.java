272a49368672b638ec22daab664975add6457113
BoscoZZZ <94723520+BoscoZZZ@users.noreply.github.com>
2024-09-09 09:16:36 -0700

    Mark2

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Multiples {
+public class Multiples {
+    public static void main(String[] args) {
+        int result = 0;
+        doMultiples(result);
+    }
+
+    private static void doMultiples(int result) {
+        for (int i = 1; i < 1000; i++) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            // Print our appropriate result.
+            if (divisibleBy3 || divisibleBy5) {
+                System.out.println(i);
+                result++;
+            }
+        }
+    }
 }
--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int result = 0;
+        for(int i = 100; i > -1; i--){
+            if (i % 2 == 0) {
+                i = i / 2;
+                System.out.println(i);
+                result++;
+            }else{
+                i = i - 1;
+                System.out.println(i);
+                result++;
+            }
+        }
+        System.out.println(result);
+
+    }
 }
